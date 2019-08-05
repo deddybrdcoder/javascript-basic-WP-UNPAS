6f89a2c703328c0ecb208eae64f82a7f53949831
deddybrdcoder <dedi.triyadi.barnawan@gmail.com>
2019-08-05 12:05:23 +0700

    edit yang salah

--- script.js
+++ script.js
@@ -65,3 +65,16 @@
 //     console.log('Yang anda masukan bukan ANGKA');
 // }
 
+var jmlAngkot = 10;
+var angkotBeroperasi = 6;
+var noAngkot = 1;
+
+for (noAngkot =1; noAngkot <= jmlAngkot; noAngkot++) {
+    
+    if (noAngkot <= angkotBeroperasi) {
+        console.log('Angko No.' +noAngkot+ ' beroperasi dengan baik.');
+        
+    } else {
+        console.log('Angkot No. ' +noAngkot+ ' sedang tidak beroprasi.');
+    }
+}
