71b5839b3fa9972199a591fc392f77364407a35b
deddybrdcoder <dedi.triyadi.barnawan@gmail.com>
2019-08-06 15:15:00 +0700

    Tambahan bootstrap dan latihan

---
+++ suwit.js
@@ -0,0 +1,44 @@
+var tanya = true;
+while(tanya) {
+    // menangkap pilihan player
+    var p = prompt('pilih : gajah, semut, orang');
+
+    // menangkap pilihan computer
+    // membangkitkan bilangan random
+
+    var comp = Math.random();
+
+    if (comp < 0.34 ) {
+        comp = 'gajah';
+    } else if ( comp >= 0.34 && comp < 0.67 ) {
+        comp = 'orang';
+    } else {
+        comp = 'semut';
+    }
+
+    var hasil = '';
+    // menentukan rules
+
+    if ( p == comp ) {
+        hasil = 'SERI';
+    } else if ( p == 'gajah') {
+
+
+
+
+        hasil = (comp == 'orang') ? 'MENANG' : 'KALAH';
+    } else if (p == 'orang') {
+        hasil = (comp == 'gajah') ? 'MENANG' : 'KALAH';
+    } else if ( p == 'semut') {
+        hasil = (comp == 'orang') ? 'MENANG' : 'KALAH';
+    } else {
+        hasil = 'Memasukan pilihan yang salah!!!';
+    }
+
+    // Tampilkan hasilnya
+
+    alert('Kamu memilih : ' + p + 'dan komputer memilih : ' + comp + '\nmaka hasilnya : kamu ' + hasil);
+    tanya = confirm('lagi??');
+}
+
+alert('Terima kasih sudah bermain');
